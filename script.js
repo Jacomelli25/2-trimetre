9b20270a424b9d783c546dd4ec04478bc1fb43b0
Jacomelli25 <128715807+Jacomelli25@users.noreply.github.com>
2024-05-28 08:34:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enuciado: "Pergunta 1", 
+    alternativas: [
+       "Alternativa 1",
+      "alternativa 2",
+      ]
+  },
+   {
+    enuciado: "Pergunta 2", 
+    alternativas: [
+       "Alternativa 1",
+      "alternativa 2",
+      ]
+  },
+   {
+    enuciado: "Pergunta 3", 
+    alternativas: [
+       "Alternativa 1",
+      "alternativa 2",
+      ]
+  },
+   {
+    enuciado: "Pergunta 4", 
+    alternativas: [
+       "Alternativa 1",
+      "alternativa 2",
+      ]
+  },
+   {
+    enuciado: "Pergunta 5", 
+    alternativas: [
+       "Alternativa 1",
+      "alternativa 2",
+      ]
+  },
+];  
